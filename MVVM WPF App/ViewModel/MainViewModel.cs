d5fd80a3c81f34cb64336fa1192abc6079510cd8
Jacob Enon <jacobenon@hotmail.com>
2020-07-08 22:14:33 +0100

    Initial commit

---
+++ MVVM WPF App/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BigJacob.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM_WPF_App.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Properties
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
